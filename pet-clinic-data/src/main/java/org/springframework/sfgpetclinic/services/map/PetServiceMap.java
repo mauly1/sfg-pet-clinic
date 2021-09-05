55d0a7b8b99e2bc5ebf4f0b887fcffea664f506b
sunil kumar maurya <sunilkumar.maurya@team.telstra.com>
2021-09-05 07:21:11 +0530

    add map serevices

---
+++ pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package org.springframework.sfgpetclinic.services.map;
+
+import org.springframework.sfgpetclinic.model.Pet;
+import org.springframework.sfgpetclinic.model.Vet;
+import org.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findaAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
